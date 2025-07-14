ec00c2672b42125dde0422cc8c47f24fe0d9f89a
gaurav thapa <gaurav16547@gmail.com>
2025-07-14 11:43:51 +0545

    initial api routes file

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/posts', [PostController::class, 'index'])->name('post-index');
 Route::get('/posts/{post}', [PostController::class, 'show']) ->name('post-show');
 Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('post-destroy');
+
+
+Route::get('/test', function () {
+    return ['message' => 'API working'];
+});
