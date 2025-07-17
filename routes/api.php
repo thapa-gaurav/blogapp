63813f8a36bebf5f972376bf61a7dc3ec8c9b244
gaurav thapa <gaurav16547@gmail.com>
2025-07-17 16:15:39 +0545

    added more endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\PasswordController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,5 +16,13 @@ Route::patch('/posts/{postID}', [PostController::class, 'update'])->name('post-u
 //auth
 Route::post('/login', [SessionController::class, 'store'])->name('login-store');
 Route::post('/logout', [SessionController::class, 'destroy'])->name('logout')->middleware(['auth:sanctum']);
+
+//register
+Route::post('/register', [RegisteredUserController::class, 'store'])->name('register-store');
+
+
+//password
+Route::patch('/password/update', [PasswordController::class, 'update'])->middleware(['auth:sanctum'])->name('password-update');
+
 
 
